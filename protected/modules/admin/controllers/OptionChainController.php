e8f7d635297eb282042c9420830f2c002cfb6051
Anand <anandcastro221@gmail.com>
2023-06-04 19:14:18 +0530

    Sql query changes

--- protected/modules/admin/controllers/OptionChainController.php
+++ protected/modules/admin/controllers/OptionChainController.php
@@ -32,6 +32,7 @@ class OptionChainController extends Controller
 
         //$query = (new Query())->select(['*'])->from('option-chain')->where(['type' => $type]);
         $connection = Yii::$app->getDb();
+        if($current_date !== '') {
         $command = $connection->createCommand('SELECT * FROM `option-chain` 
 WHERE type= "' . $type . '" AND strike_price BETWEEN 
 ' . $from_strike . ' AND ' . $to_strike . ' AND expiry_date = "' . $expiry_date . '" 
@@ -41,6 +42,14 @@ AND (CONVERT(DATE_FORMAT(FROM_UNIXTIME(`created_at`), "%H"), DECIMAL) >= 9)
     AND created_at BETWEEN 
     ' . strtotime(date('Y-m-d 00:00:00', strtotime(str_replace('/', '-', $current_date)))) . ' AND 
     ' . strtotime(date('Y-m-d 23:59:59', strtotime(str_replace('/', '-', $current_date)))));
+        } else {
+             $command = $connection->createCommand('SELECT * FROM `option-chain` 
+WHERE type= "' . $type . '" AND strike_price BETWEEN 
+' . $from_strike . ' AND ' . $to_strike . ' AND expiry_date = "' . $expiry_date . '" 
+AND MOD(TIMESTAMPDIFF(MINUTE,concat(DATE(FROM_UNIXTIME(`created_at`)), " ", "09:15:00"), FROM_UNIXTIME (created_at)), ' . $min . ') = 0
+AND TIMESTAMPDIFF(MINUTE,concat(DATE(FROM_UNIXTIME(`created_at`)), " ", "09:16:00"), FROM_UNIXTIME (created_at)) >= 0
+AND (CONVERT(DATE_FORMAT(FROM_UNIXTIME(`created_at`), "%H"), DECIMAL) >= 9)');
+        }
         // print_r('SELECT * FROM `option-chain` 
         // WHERE type= "' . $type . '" AND strike_price BETWEEN ' . $from_strike . ' AND ' . $to_strike . ' AND expiry_date = "' . $expiry_date . '" AND (CONVERT(DATE_FORMAT(FROM_UNIXTIME(`created_at`), "%i"), DECIMAL) % ' . $min . ') = 0 AND created_at BETWEEN ' . strtotime(date('Y-m-d 00:00:00', strtotime(str_replace('/', '-', $current_date)))) . ' AND ' . strtotime(date('Y-m-d 23:59:59', strtotime(str_replace('/', '-', $current_date)))));
         $result = $command->queryAll();
@@ -53,7 +62,7 @@ AND (CONVERT(DATE_FORMAT(FROM_UNIXTIME(`created_at`), "%H"), DECIMAL) >= 9)
                     'ce_oi' => $res['ce_oi'],
                     'pe_oi' => $res['pe_oi'],
                     'pe_oi_change' => $res['pe_oi_change'],
-                    'date_format' => date('H:i', $res['created_at'])
+                    'date_format' => date('d M H:i', $res['created_at'])
                 ];
             }
         }
